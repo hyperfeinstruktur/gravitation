b7790cdbfb4d31610b5190b3871ff6e6cf9c1fc9
Patrick Hirling <90903147+hyperfeinstruktur@users.noreply.github.com>
2022-02-28 01:00:11 +0100

    Add Plummer IC script

---
+++ ics/isotropic_plummer.py
@@ -0,0 +1,86 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle as pkl
+
+G= 4.30091e-3 # pc / solar mass * (kms)^2
+
+def r_of_m(m,a,M):
+    return a * ((M/m)**(2./3.) - 1)**(-1./2.)
+
+def relative_potential(r,a,M): # = - phi
+    return G*M / np.sqrt(r**2 + a**2)
+
+def relative_energy(r,v,a,M):
+    return relative_potential(r,a,M) - 0.5 * v**2
+
+def DF(r,v,a,M):
+    A = 24. * np.sqrt(2) / (7*np.pi)
+    eps = relative_energy(r,v,a,M)
+    if eps < 0: return 0
+    else : return A * G**(-5.) * M**(-4.) * a**2 * eps**(3.5)
+
+DF_vect = np.vectorize(DF)
+
+def v_max(r,a,M):
+    return np.sqrt(2.*relative_potential(r,a,M))
+
+def sample_velocity(r,a,M):
+    v_e = v_max(r,a,M)
+    x = 0.
+    y = 0.
+    while True:
+        x = v_e*np.random.uniform(0.,1.)
+        y = DF(r,0.,a,M)*np.random.uniform(0.,1.) # The DF is maximal at r=0 (analytic)
+        if y < DF(r,x,a,M) and x < v_max(r,a,M): return x,y
+
+
+
+# Model Parameters
+a = 5     # pc
+M = 4.0e4  # solar mass
+N = 4000    #-
+
+# Generate Positions
+m_rand = M*np.random.uniform(0.0,1.0,N)
+r_rand = r_of_m(m_rand,a,M)
+phi_rand = np.random.uniform(0.0,2*np.pi,N)
+theta_rand = np.arccos( np.random.uniform(-1.,1.,N) )
+
+x = r_rand * np.sin(theta_rand) * np.cos(phi_rand)
+y = r_rand * np.sin(theta_rand) * np.sin(phi_rand)
+z = r_rand * np.cos(theta_rand)
+
+X = np.array([x,y,z]).transpose()
+print(np.amax(np.sqrt(np.sum(X**2,1))))
+# Generate Velocities
+vel_rand = np.empty(N)
+for i in range(len(vel_rand)):
+    vel_rand[i] = sample_velocity(r_rand[i],a,M)[0]
+
+phi_rand = np.random.uniform(0.0,2*np.pi,N)
+theta_rand = np.arccos( np.random.uniform(-1.,1.,N) )
+
+v_x = vel_rand * np.sin(theta_rand) * np.cos(phi_rand)
+v_y = vel_rand * np.sin(theta_rand) * np.sin(phi_rand)
+v_z = vel_rand * np.cos(theta_rand)
+
+V = np.array([v_x,v_y,v_z]).transpose()
+print(np.amax(V))
+# Generate masses
+m = M/N * np.ones(N)
+#print(m)
+
+# Pickle results
+if 1:
+    with open('X.pkl','wb') as f:
+        pkl.dump(X,f)
+
+    with open('V.pkl','wb') as f:
+        pkl.dump(V,f)
+
+    with open('M.pkl','wb') as f:
+        pkl.dump(m,f)
+
+
+plt.scatter(x,y,s=0.4)
+plt.show()
