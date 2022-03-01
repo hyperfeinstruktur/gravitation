4c6a7db86dc3a90e21eaa27aac73e65b9d868d72
Patrick Hirling <90903147+hyperfeinstruktur@users.noreply.github.com>
2022-03-01 23:00:20 +0100

    Cleanup

--- ics/isotropic_plummer.py
+++ ics/isotropic_plummer.py
@@ -2,10 +2,33 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pickle as pkl
 
-G= 4.30091e-3 # pc / solar mass * (kms)^2
+
+
+####### Model Parameters
+
+G = 4.30091e-3 # pc / solar mass * (kms)^2
+a = 10         # pc
+M = 6.0e4      # solar mass
+N = 200        # -
+
+####### Generate Positions
 
 def r_of_m(m,a,M):
     return a * ((M/m)**(2./3.) - 1)**(-1./2.)
+
+m_rand = M*np.random.uniform(0.0,1.0,N)
+r_rand = r_of_m(m_rand,a,M)
+phi_rand = np.random.uniform(0.0,2*np.pi,N)
+theta_rand = np.arccos( np.random.uniform(-1.,1.,N) )
+
+x = r_rand * np.sin(theta_rand) * np.cos(phi_rand)
+y = r_rand * np.sin(theta_rand) * np.sin(phi_rand)
+z = r_rand * np.cos(theta_rand)
+
+X = np.array([x,y,z]).transpose()
+print(np.amax(np.sqrt(np.sum(X**2,1))))
+
+####### Generate Velocities
 
 def relative_potential(r,a,M): # = - phi
     return G*M / np.sqrt(r**2 + a**2)
@@ -37,26 +60,6 @@ def sample_velocity(r,a,M):
         X5 = np.random.uniform()
         if 0.1*X5 < (X4**2*(1.-X4**2)**(3.5)): return X4*v_e, X5*v_e
 
-
-
-# Model Parameters
-a = 10     # pc
-M = 6.0e4  # solar mass
-N = 200    #-
-
-# Generate Positions
-m_rand = M*np.random.uniform(0.0,1.0,N)
-r_rand = r_of_m(m_rand,a,M)
-phi_rand = np.random.uniform(0.0,2*np.pi,N)
-theta_rand = np.arccos( np.random.uniform(-1.,1.,N) )
-
-x = r_rand * np.sin(theta_rand) * np.cos(phi_rand)
-y = r_rand * np.sin(theta_rand) * np.sin(phi_rand)
-z = r_rand * np.cos(theta_rand)
-
-X = np.array([x,y,z]).transpose()
-print(np.amax(np.sqrt(np.sum(X**2,1))))
-# Generate Velocities
 vel_rand = np.empty(N)
 for i in range(len(vel_rand)):
     vel_rand[i] = sample_velocity(r_rand[i],a,M)[0]
@@ -70,22 +73,21 @@ v_z = vel_rand * np.cos(theta_rand)
 
 V = np.array([v_x,v_y,v_z]).transpose()
 print(np.amax(V))
-# Generate masses
-m = M/N * np.ones(N)
-#print(m)
 
-# Pickle results
+####### Generate masses & Pickle results
+
+m = M/N * np.ones(N)
 if 1:
     with open('X.pkl','wb') as f:
         pkl.dump(X[:],f)
 
     with open('V.pkl','wb') as f:
         pkl.dump(V[:],f)
-        #pkl.dump(np.zeros(V.shape),f)
 
     with open('M.pkl','wb') as f:
         pkl.dump(m[:],f)
 
-
-# plt.scatter(x,y,s=0.4)
-# plt.show()
+####### Optional: Display xy projection
+if 1:
+    plt.scatter(x,y,s=0.4)
+    plt.show()
